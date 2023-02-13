96b1d83037cc33effe238c3748ea6fa1e7b9ca7d
Pol Ventura Pont <polventurapont@gmail.com>
2023-02-13 17:33:33 +0100

    First Menu

---
+++ Activity4.py
@@ -0,0 +1,16 @@
+opcio = 0
+flag = True
+
+while flag == True:
+    
+    print("""
+    QUÈ VOLS CALCULAR
+    
+    1) Heads and Tails
+    2) Rock, Paper, Scissors
+    3) Evens and Odds
+    4) Memory Test
+    6) EXIT
+    """)
+    
+    opcio = int(input("TRIA UNA OPCIÓ --> "))
