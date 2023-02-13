a397eb5b065d914348dddd74f045c327952c412e
Pol Ventura Pont <polventurapont@gmail.com>
2023-02-13 17:40:30 +0100

    Menu with options

--- Activity4.py
+++ Activity4.py
@@ -1,16 +1,35 @@
+#Pol Ventura
+def DoHeadsAndTails():
+    
+
+#Pol Besalú
+
+#Martí Roura
+
+
+
+
+
+
 opcio = 0
 flag = True
 
 while flag == True:
     
     print("""
-    QUÈ VOLS CALCULAR
+    WHAT DO YOU WANT TO PLAY?
     
     1) Heads and Tails
     2) Rock, Paper, Scissors
     3) Evens and Odds
     4) Memory Test
-    6) EXIT
+    0) EXIT
     """)
     
-    opcio = int(input("TRIA UNA OPCIÓ --> "))
+    opcio = int(input("CHOOSE AN OPTIONS --> "))
+    if opcio == 1:
+        DoHeadsAndTails()
+    elif opcio == 0:
+        flag = False
+        
+print("END OF THE GAME, THANK YOU FOR PLAYING!!!")
