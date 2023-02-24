481022df12b4fe6b5fca33f606a68db788e53f9e
Pol Besalu <polbesalu@gmail.com>
2023-02-24 19:20:39 +0100

    Second_commit_PB

--- Activity4.py
+++ Activity4.py
@@ -29,6 +29,10 @@ def play():
 
     return 'You lost!'
 
+def is_win(player, opponent):
+    
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
 
 
 print(play())
